21472900bc24021e927d123356a5585146c2c090
saq1rette <165058154+saq1rette@users.noreply.github.com>
2026-05-31 22:48:58 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,87 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2025
+
 #include "train.h"
+
+Train::Train() {
+    first = nullptr;
+    countOp = 0;
+}
+
+Train::~Train() {
+    if (first == nullptr) {
+        return;
+    }
+
+    Car* current = first->next;
+
+    while (current != first) {
+        Car* temp = current;
+        current = current->next;
+        delete temp;
+    }
+
+    delete first;
+}
+
+void Train::addCar(bool light) {
+    Car* newCar = new Car;
+
+    newCar->light = light;
+
+    if (first == nullptr) {
+        first = newCar;
+
+        first->next = first;
+        first->prev = first;
+
+        return;
+    }
+
+    Car* last = first->prev;
+
+    newCar->next = first;
+    newCar->prev = last;
+
+    last->next = newCar;
+    first->prev = newCar;
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
+
+int Train::getLength() {
+    if (first == nullptr) {
+        return 0;
+    }
+
+    countOp = 0;
+
+    Car* current = first;
+
+    current->light = true;
+
+    while (true) {
+        int distance = 0;
+
+        do {
+            current = current->next;
+
+            countOp++;
+
+            distance++;
+        } while (!current->light);
+
+        current->light = false;
+
+        for (int i = 0; i < distance; i++) {
+            current = current->prev;
+
+            countOp++;
+        }
+
+        if (!current->light) {
+            return distance;
+        }
+    }
+}
